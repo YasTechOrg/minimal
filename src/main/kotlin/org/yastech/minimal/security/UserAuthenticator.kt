4742ba00dbaf4da6aeeedaa61f4f5c9d90a769ab
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-09-27 01:02:37 +0330

    Fix User Authenticator Bugs

--- src/main/kotlin/org/yastech/minimal/security/UserAuthenticator.kt
+++ src/main/kotlin/org/yastech/minimal/security/UserAuthenticator.kt
@@ -1,4 +1,4 @@
-package org.yastech.weeki.security
+package org.yastech.minimal.security
 
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
