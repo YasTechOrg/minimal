ca5a01f8a93f2b7812c443d97a234353b141d313
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-09-27 01:02:49 +0330

    Update User Service

--- src/main/kotlin/org/yastech/minimal/services/UserService.kt
+++ src/main/kotlin/org/yastech/minimal/services/UserService.kt
@@ -1,13 +1,17 @@
 package org.yastech.minimal.services
 
 import org.springframework.stereotype.Service
+import org.yastech.minimal.data.PasswordEncoder
 import org.yastech.minimal.tables.User
 import org.yastech.minimal.tables.UserRepository
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class UserService
 (
     private var repository: UserRepository,
+    private var passwordEncoder: PasswordEncoder
 )
 {
     fun get(id: String): User
@@ -18,5 +22,18 @@ class UserService
     fun exist(id: String): Boolean
     {
         return repository.existsById(id).block()!!
+    }
+
+    fun add(user: User)
+    {
+        user.password = passwordEncoder.encoder().encode(user.password)
+
+        user.forgot = false
+
+        user.forgotCode = null
+
+        user.accepted = false
+
+        repository.save(user).subscribe()
     }
 }
