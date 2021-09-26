1909fffe22b77b774c75b9cc93d97bc5069946db
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-09-26 20:41:47 +0330

    Update User Service

--- src/main/kotlin/org/yastech/minimal/services/UserService.kt
+++ src/main/kotlin/org/yastech/minimal/services/UserService.kt
@@ -1,6 +1,7 @@
 package org.yastech.minimal.services
 
 import org.springframework.stereotype.Service
+import org.yastech.minimal.tables.User
 import org.yastech.minimal.tables.UserRepository
 
 @Service
@@ -9,5 +10,13 @@ class UserService
     private var repository: UserRepository,
 )
 {
+    fun get(id: String): User
+    {
+        return repository.findById(id).block()!!
+    }
 
+    fun exist(id: String): Boolean
+    {
+        return repository.existsById(id).block()!!
+    }
 }
