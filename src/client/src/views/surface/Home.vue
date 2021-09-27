a76cbd7c145cfbaab13d39726ef71ff5ba828be2
sahar_hallaji <saharHallaji.dev@gmail.com>
2021-09-27 20:48:30 +0330

    Home -> Search Input ( mobile & tablet -> Complete)

--- src/client/src/views/surface/Home.vue
+++ src/client/src/views/surface/Home.vue
@@ -3,11 +3,11 @@
 
 #home
   .inSalesProducts
-    .input-group.w-50.me-auto.ms-auto.d-flex.justify-content-between.position-relative
+    .input-group.searchInput.w-50.me-auto.ms-auto.d-flex.justify-content-between.position-relative
       input.form-control( placeholder="Search everything you want....")
+      input.form-control.d-none( placeholder="Search...")
       img.input-group-text( src="../../assets/img/images/search.png" alt="Search Icon" )
-  p IN SALES PRODUCTS
-
+    p IN SALES PRODUCTS
 
 </template>
 
