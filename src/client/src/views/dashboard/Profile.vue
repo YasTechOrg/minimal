32de5226c07c6e8e4e362476a31799f0a8ce2e82
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-09-29 23:21:16 +0330

    Add Dashboard Pages

---
+++ src/client/src/views/dashboard/Profile.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+
+#profile
+
+
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+
+})
+
+export default class Profile extends Vue {}
+</script>
---
+++ src/client/src/views/dashboard/Purchase.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+
+#purchase
+
+
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+
+})
+
+export default class Purchase extends Vue {}
+</script>
