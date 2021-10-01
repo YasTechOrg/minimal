aab597451dee0b6460ac0f5731bafbd630d557cd
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-10-01 17:01:35 +0330

    Remove Stupid Auth Manager Add Persisted State Plugin To Vuex Design Auth Model In Vuex Add Smart Auth Methods

--- src/client/src/store/index.ts
+++ src/client/src/store/index.ts
@@ -1,12 +1,47 @@
 import { createStore } from 'vuex'
+import createPersistedState from "vuex-persistedstate"
 
 export default createStore({
   state: {
+    userData: {},
+    at: "",
+    at_time: ""
   },
+
+  plugins: [createPersistedState()],
+
   mutations: {
+    setAuth(state, token)
+    {
+      state.at = token
+      state.at_time = String(new Date())
+    },
+
+    removeAuth(state)
+    {
+      state.at = ""
+      state.at_time = ""
+      state.userData = {}
+    },
+
+    setUserData(state, data)
+    {
+      state.userData = data
+    }
   },
+
+  getters: {
+    getAuth: state =>
+    {
+      return `Bearer ${state.at}`
+    }
+  },
+
   actions: {
+
   },
+
   modules: {
+
   }
 })
