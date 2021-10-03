45a5966b299337569b72a1ada404465a51c57709
Hossein Araghi <hoseinaraghi84@gmail.com>
2021-10-03 14:45:59 +0330

    Add Search Socket Controller

---
+++ src/main/kotlin/org/yastech/minimal/controllers/SearchController.kt
@@ -0,0 +1,49 @@
+package org.yastech.minimal.controllers
+
+import com.google.gson.Gson
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.stereotype.Controller
+import org.yastech.minimal.dispatcher.SearchDispatcher
+import org.yastech.minimal.models.SearchModel
+import org.yastech.minimal.services.ProductService
+
+
+@Controller
+class SearchController
+(
+    private val searchDispatcher: SearchDispatcher,
+    private val productService: ProductService
+)
+{
+    @Suppress("UNCHECKED_CAST")
+    @MessageMapping("/do-search")
+    fun start(stompHeaderAccessor: StompHeaderAccessor)
+    {
+        // Get Search Values And Convert To Map
+        val searchItems = Gson()
+            .fromJson(
+                stompHeaderAccessor.getNativeHeader("search")!![0].toString(),
+                Map::class.java
+            )
+
+        // Execute Search Query
+        val searchResult = productService.doSearch(
+            SearchModel(
+                searchItems["text"].toString(),
+                searchItems["categories"] as MutableList<String>,
+                searchItems["brands"] as MutableList<String>,
+                searchItems["min_price"] as Long,
+                searchItems["max_price"] as Long,
+            )
+        )
+
+        // Convert Result To Json
+        val jsonSearchResult = Gson()
+            .toJson(searchResult)
+
+        // Send Result To Client
+        searchDispatcher.send(stompHeaderAccessor.sessionId!!, jsonSearchResult)
+
+    }
+}
