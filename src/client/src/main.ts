3dd7d6b9837a46ba5c796ef2383bd52c13e77ec6
Yas Tech <yastechorg@gmail.com>
2021-10-05 14:39:27 +0330

    Install And Config Search Page Prerequisites

--- src/client/src/main.ts
+++ src/client/src/main.ts
@@ -6,7 +6,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import "@/assets/lib/w3.css"
 import "@/assets/lib/google_icons.css"
-import 'vue3-carousel/dist/carousel.css';
+import "vue3-carousel/dist/carousel.css"
+import "@vueform/slider/themes/default.css"
+import "@vueform/multiselect/themes/default.css"
 
 const app = createApp(App)
 app.use(store)
